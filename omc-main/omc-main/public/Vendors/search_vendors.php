1baf4038bfcd668b548b5e830b43f2c69b2ee3c2
Jack <brawley.jv@gmail.com>
2025-03-30 17:20:11 -0500

    Delete omc-main directory

--- omc-main/omc-main/public/Vendors/search_vendors.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/Config.php';
-require_once BASE_PATH . '/Models/Database.php';
-require_once BASE_PATH . '/Controllers/VendorController.php';
-
-// ...existing code...
-?>
